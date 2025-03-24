80d5e685c35ceed988340900314bccccf4dadab5
wimdard <lololog999@gmail.com>
2025-03-24 22:13:07 +0100

    Day1 Update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
+        int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QPushButton button("Закрыть");
+    QObject::connect(&button, &QPushButton::clicked, &app, &QApplication::quit);
+
     button.show();
-    return QApplication::exec();
+
+    return app.exec();
 }
+
